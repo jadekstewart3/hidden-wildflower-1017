86357ff228855c9e4e0bf12ddf7d57e365e57847
Jade Stewart <jadekstewart3@gmail.com>
2023-09-26 10:58:44 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :scientists, only: :show
+  resources :scientists, only: :show do 
+    resources :scientists_experiments, only: :destroy
+  end
 end
